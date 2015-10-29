80769bf47e219a69f892841277eaaf7efaff9911
sunkink29 <calebparks29@gmail.com>
2015-10-29 08:25:45 -0500

    added ability to switch control scheme
    added the ability to switch control scheme without changing op modes

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotManualTriggerControl.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotManualTriggerControl.java
@@ -10,6 +10,8 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  */
 public class PushBotManualTriggerControl extends PushBotTelemetry {
 
+    int controller1ControlScheme = 1;
+
     //--------------------------------------------------------------------------
     //
     // PushBotManual
@@ -66,35 +68,20 @@ public class PushBotManualTriggerControl extends PushBotTelemetry {
         // class, but the power levels aren't applied until this method ends.
         //
 
-        //
-        // Manage the drive wheel motors.
-        //
-        // Old Drive control method
-        //float l_left_drive_power = scale_motor_power (-gamepad1.left_stick_y);
-        //float l_right_drive_power = scale_motor_power (-gamepad1.right_stick_y);
+        // Determine if the Contoller 1 control Scheme needs to be changed
 
-        // get the power from the right trigger
-
-        float l_left_drive_power = scale_motor_power(gamepad1.right_trigger);
-        float l_right_drive_power = scale_motor_power(gamepad1.right_trigger);
-
-        // modify the current power with the left trigger
-
-        l_left_drive_power = l_left_drive_power - scale_motor_power(gamepad1.left_trigger);
-        l_right_drive_power = l_right_drive_power - scale_motor_power(gamepad1.left_trigger);
-
-        // determine which way to turn and slow down that wheel
-
-        if (scale_motor_power(-gamepad1.right_stick_x) < 0){
-            l_left_drive_power = l_left_drive_power * -(scale_motor_power(-gamepad1.right_stick_x) - 1);
+        if (gamepad1.a) {
+        controller1ControlScheme = 1;
+        } else if (gamepad1.b) {
+            controller1ControlScheme = 2;
         }
-        else if (scale_motor_power(-gamepad1.right_stick_x) > 0){
-            l_right_drive_power = l_right_drive_power * -(scale_motor_power(gamepad1.right_stick_x) - 1);
+        // Determine Which controller 1 control Scheme to use
+
+        if (controller1ControlScheme == 1) {
+            useController1ControlScheme1();
+        } else if (controller1ControlScheme == 2) {
+            useController1ControlScheme2();
         }
-
-        // set the drive power
-
-        set_drive_power (l_left_drive_power, l_right_drive_power);
 
         //
         // Manage the arm motor.
@@ -132,5 +119,47 @@ public class PushBotManualTriggerControl extends PushBotTelemetry {
         update_gamepad_telemetry ();
 
     } // loop
+
+    public  void useController1ControlScheme1() {
+
+        // Manage the drive wheel motors.
+
+        float l_left_drive_power = scale_motor_power (-gamepad1.left_stick_y);
+        float l_right_drive_power = scale_motor_power (-gamepad1.right_stick_y);
+
+        // set the drive power
+
+        set_drive_power (l_left_drive_power, l_right_drive_power);
+    }
+
+    public  void useController1ControlScheme2() {
+
+        //
+        // Manage the drive wheel motors.
+
+        // get the power from the right trigger
+
+        float l_left_drive_power = scale_motor_power(gamepad1.right_trigger);
+        float l_right_drive_power = scale_motor_power(gamepad1.right_trigger);
+
+        // modify the current power with the left trigger
+
+        l_left_drive_power = l_left_drive_power - scale_motor_power(gamepad1.left_trigger);
+        l_right_drive_power = l_right_drive_power - scale_motor_power(gamepad1.left_trigger);
+
+        // determine which way to turn and slow down that wheel
+
+        if (scale_motor_power(-gamepad1.right_stick_x) < 0){
+            l_left_drive_power = l_left_drive_power * -(scale_motor_power(-gamepad1.right_stick_x) - 1);
+        }
+        else if (scale_motor_power(-gamepad1.right_stick_x) > 0){
+            l_right_drive_power = l_right_drive_power * -(scale_motor_power(gamepad1.right_stick_x) - 1);
+        }
+
+        // set the drive power
+
+        set_drive_power (scale_motor_power(l_left_drive_power), scale_motor_power(l_right_drive_power));
+
+    }
 
 } // PushBotManual
