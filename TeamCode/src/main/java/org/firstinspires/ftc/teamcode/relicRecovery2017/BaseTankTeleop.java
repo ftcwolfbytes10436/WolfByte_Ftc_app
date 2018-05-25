4a5572e006ef365f8501dbc253bce6c59f30c1e8
HeyBobBrown <HeyBobBrown@gmail.com>
2018-05-24 21:50:02 -0500

    File Save v1.0 (will not work)
    Bob created this at the programming session at McD's. It needs the file path to be updated to actually work. It starts recording when the "a" button is pressed and stops recording when the "b" button is pressed. Both on Controller 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/relicRecovery2017/BaseTankTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/relicRecovery2017/BaseTankTeleop.java
@@ -5,8 +5,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
+// For Saving Joystick values
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.lang.Thread;
 
 @TeleOp(name ="tank_teleop")
+
+
 
 public class BaseTankTeleop extends OpMode{
 
@@ -15,6 +24,21 @@ public class BaseTankTeleop extends OpMode{
     boolean triggerHit = false;
     double liftMotorPower = 0;
 
+
+    // For Saving Joystick values
+    ArrayList<Float> LeftValues = new ArrayList<>();
+    ArrayList<Float> RightValues = new ArrayList<>();
+    ArrayList<Long> TimeValues = new ArrayList<>();
+
+    File f;
+    BufferedWriter bw;
+    FileWriter fw;
+
+    long StartMillis;
+
+    boolean Recording;
+
+
     @Override
     public void init() {
         robot.init(hardwareMap, telemetry);
@@ -22,15 +46,21 @@ public class BaseTankTeleop extends OpMode{
         telemetry.addData("Status", "waiting to start");
         telemetry.update();
 
+        // For Saving Joystick values
+        StartMillis = System.currentTimeMillis();
+        Recording = false;
+
         robot.LeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.RightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         robot.LeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         robot.RightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
     }
 
     @Override
     public void loop() {
+
         robot.LeftMotor.setPower(Range.clip(gamepad1.left_stick_y, -1, 1));
         robot.RightMotor.setPower(Range.clip(gamepad1.right_stick_y, -1, 1));
 
@@ -53,7 +83,7 @@ public class BaseTankTeleop extends OpMode{
         }
         else //lowering
         {
-            liftMotorPower = liftMotorPower * .4; // we want 10% of the joytick value for power (slow down the lowering)
+            liftMotorPower = liftMotorPower * .1; // we want 10% of the joytick value for power (slow down the lowering)
         }
 
         robot.LifterMotor.setPower(robot.backFeedPower + liftMotorPower);
@@ -75,6 +105,54 @@ public class BaseTankTeleop extends OpMode{
         {
             robot.setGriperPos(1);
         }
+
+        // For Saving Joystick values
+
+        if (gamepad1.a == true) {
+            Recording = true;
+            LeftValues.clear();
+            RightValues.clear();
+            TimeValues.clear();
+            StartMillis = System.currentTimeMillis();
+        }
+
+        if (Recording) { //Only record if a has been pressed to start recording, b stops recording
+            try {
+                LeftValues.add(gamepad1.left_stick_y);
+                RightValues.add(gamepad1.right_stick_y);
+                TimeValues.add(System.currentTimeMillis() - StartMillis);
+
+                if (gamepad1.b == true) {
+                    try {
+                        f = new File("/home/lvuser/Output.txt");
+                        if (!f.exists()) {
+                            f.createNewFile();
+                        }
+                        fw = new FileWriter(f);
+                        bw = new BufferedWriter(fw);
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    for (int x = 0; x < LeftValues.size(); x++) {
+                        bw.write(TimeValues.get(x) + "," + LeftValues.get(x) + "," + RightValues.get(x) + "\n");
+                    }
+
+                    bw.close();
+                    fw.close();
+                    Thread.sleep(1000); //give them time to release the button.
+                }
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Recording = false;
+        }
     }
 }
+
 
